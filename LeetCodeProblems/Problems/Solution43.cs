7670164e034ba4c74bb0476e1b21494632075916
Michele Corazza <info@michelecorazza.name>
2022-09-03 23:10:12 +0200

    Solution 43

---
+++ LeetCodeProblems/Problems/Solution43.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Text;
+
+namespace LeetCodeProblems.Problems
+{
+    internal class Solution43
+    {
+        public string Multiply(string num1, string num2)
+        {
+            int[] inum1 = num1
+                    .Where(n => char.IsDigit(n))
+                    .Select(n => int.Parse(n.ToString()))
+                    .ToArray();
+
+            int[] inum2 = num2
+                    .Where(n => char.IsDigit(n))
+                    .Select(n => int.Parse(n.ToString()))
+                    .ToArray();
+
+            int[] product = new int[num1.Length + num2.Length];
+
+            for (int i = 0; i < num1.Length; i++)
+                for (int j = 0; j < num2.Length; j++)
+                    product[i + j + 1] += inum1[i] * inum2[j];
+
+            for (int i = product.Length - 1; i > 0; i--)
+            {
+                product[i - 1] += product[i] / 10;
+                product[i] %= 10;
+            }
+
+            int pi = 0;
+            while (pi < product.Length && product[pi] == 0)
+            {
+
+                if (++pi >= product.Length)
+                    return "0";
+            }
+
+            StringBuilder sb = new();
+            for (; pi < product.Length; pi++)
+                sb.Append(product[pi]);
+
+            return sb.ToString();
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -1,34 +1,9 @@
 ﻿using LeetCodeProblems.Problems;
 
 
-ListNode list1 = new()
-{
-    val = 2,
-    next = new ListNode()
-    {
-        val = 4,
-        next = new ListNode()
-        {
-            val = 3,
+Solution43 solution = new();
 
-        }
-    }
-};
-ListNode list2 = new()
-{
-    val = 5,
-    next = new ListNode()
-    {
-        val = 6,
-        next = new ListNode()
-        {
-            val = 4
-        }
-    }
-};
 
-Solution2 solution = new();
-
-var dd = solution.AddTwoNumbers(list1, list2);
+var dd = solution.Multiply("123456789", "987654321");
 
 Console.ReadLine();
