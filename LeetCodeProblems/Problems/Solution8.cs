fb9eab3f1623b2682f931a4e652910d2ed1cd07c
Michele Corazza <info@michelecorazza.name>
2022-09-04 16:41:23 +0200

    Soluzion 8

---
+++ LeetCodeProblems/Problems/Solution8.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Text;
+
+namespace LeetCodeProblems.Problems;
+
+internal class Solution8
+{
+    public int MyAtoi(string s)
+    {
+        s = s.Trim();
+
+        if (string.IsNullOrEmpty(s))
+            return 0;
+
+        var array = s.ToArray();
+
+        var sign = 1;
+        var index = 0;
+
+        StringBuilder builder = new();
+
+        if ((array[index] == '-') || (array[index] == '+'))
+        {
+            sign = array[index] == '-' ? -1 : 1;
+            index++;
+        }
+
+        while (index < array.Length)
+        {
+            if (char.IsDigit(array[index]))
+            {
+                builder.Append(array[index]);
+            }
+            else
+                break;
+
+            index++;
+        }
+
+        try
+        {
+            if (string.IsNullOrEmpty(builder.ToString()))
+                return 0;
+
+            return int.Parse(builder.ToString()) * sign;
+        }
+        catch
+        {
+            return sign < 0 ? int.MinValue : int.MaxValue;
+        }
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -1,18 +1,10 @@
 ﻿using LeetCodeProblems.Problems;
 
 
-Solution26 solution = new();
+Solution8 solution = new();
 
-int[] nums = { 1, 1, 2 }; // Input array
-int[] expectedNums = { 1, 2 }; // The expected answer with correct length
+string s = "   -42";
 
-int k = Solution26.RemoveDuplicates(nums); // Calls your implementation
-
-for (int i = 0; i < expectedNums.Length; i++)
-{
-    var b = nums[i] == expectedNums[i];
-}
-
-//var dd = solution.RemoveDuplicates(nums);
+var dd = solution.MyAtoi(s);
 
 Console.ReadLine();
