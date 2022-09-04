bd6a65b6df9735d3157ecf7103fd96601ac9bae5
Michele Corazza <info@michelecorazza.name>
2022-09-04 15:59:16 +0200

    Soluzione 26

---
+++ LeetCodeProblems/Problems/Solution26.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LeetCodeProblems.Problems;
+
+internal class Solution26
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int j = 0;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] != nums[i + 1])
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+
+
+        }
+        nums[j++] = nums[nums.Length - 1];
+
+        return j;
+    }
+}
--- LeetCodeProblems/Program.cs
+++ LeetCodeProblems/Program.cs
@@ -1,10 +1,18 @@
 ﻿using LeetCodeProblems.Problems;
 
 
-int x = 213;
+Solution26 solution = new();
 
-Solution9 solution = new();
+int[] nums = { 1, 1, 2 }; // Input array
+int[] expectedNums = { 1, 2 }; // The expected answer with correct length
 
-var dd = solution.IsPalindrome(x);
+int k = Solution26.RemoveDuplicates(nums); // Calls your implementation
+
+for (int i = 0; i < expectedNums.Length; i++)
+{
+    var b = nums[i] == expectedNums[i];
+}
+
+//var dd = solution.RemoveDuplicates(nums);
 
 Console.ReadLine();
